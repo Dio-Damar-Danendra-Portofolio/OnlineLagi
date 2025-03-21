dfd2e72a8890d72ca2b09c2f5aa764831d58c377
Dio-Damar-Danendra <diodamar14102000@gmail.com>
2025-03-21 21:32:23 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,11 @@ Route::get('/', function () {
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::get('/clients', function () {
+    return view('clients');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
