cd8d2f98daaedb5982d6adb1bacf881ef37b4e77
Dio-Damar-Danendra <diodamar14102000@gmail.com>
2025-03-21 21:27:45 +0700

    Add files via upload

---
+++ resources/views/services.blade.php
@@ -0,0 +1,11 @@
+@extends('client.desain.templat')
+@section('title','Layanan Kami')
+@section('content')
+<div class="container-fluid">
+    <div class="row d-flex flex-wrap justify-content-center align-self-sm-center vh-100">
+        <div class="col-xl-12 align-self-xl-center">
+            <h1 class="text-white text-center align-items-baseline">Selamat Datang di Onlinelagi.id, Situs untuk UMKM Kecil.</h1>
+        </div>
+    </div>
+</div>
+@endsection
